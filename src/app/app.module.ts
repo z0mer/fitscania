d7e206f4d14d59840a84d5a05f60b14663d23433
z0mer <annacarolrpz@gmail.com>
2024-10-08 14:17:35 -0300

    teste inicial com side menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TegelModule } from '@scania/tegel-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TegelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
